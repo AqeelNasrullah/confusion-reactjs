5de73131e0ba17dc5b139f98e1f798e42f4dadcc
Aqeel Nasrullah <aqeelnasrulah1997@gmail.com>
2021-01-16 23:12:48 +0500

    Removed constructor from MainComponent

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -19,10 +19,6 @@ const mapStateToProps = state => {
 }
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-
-      }
     
       onDishSelect(dishId) {
         this.setState({ selectedDish: dishId});
